2009861f9a560b088d306aecd3455d7cda30cd85
bhatia-di <bhatia.di@northeastern.edu>
2021-12-08 19:16:19 -0500

    added otp and email

---
+++ src/medistop/util/OTPUtility.java
@@ -0,0 +1,19 @@
+package medistop.util;
+
+public class OTPUtility {
+
+    static String generateOTP(int otpLength)
+    {
+        String charDirectory = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        int n = charDirectory.length();
+
+        String OTP="";
+
+        for (int i = 1; i <= otpLength; i++)
+            OTP += (charDirectory.charAt((int) ((Math.random()*10) % n)));
+
+        return(OTP);
+    }
+
+
+}
