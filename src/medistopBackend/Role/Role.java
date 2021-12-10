b535d059160fabee98c128e80ec0cc4f8d2a6e00
Zeenia Singla <singla.z@northeastern.edu>
2021-12-09 19:54:45 -0600

    Enterprise and Network Created

--- src/medistopBackend/Role/Role.java
+++ src/medistopBackend/Role/Role.java
@@ -8,21 +8,24 @@ package medistopBackend.Role;
 import javax.swing.JPanel;
 import medistopBackend.UserAccount.UserAccount;
 import medistopBackend.EcoSystem;
+import medistopBackend.Enterprise.Enterprise;
+import medistopBackend.Network.Network;
 import medistopBackend.Organisation.Organisation;
 
 /**
  *
  * @author Zeenia
  */
-public class Role {
-    private String val;
+public abstract class Role {
+    
     
     public enum RoleType{
             HospitalAdmin("HospitalAdmin"), Doctor("Doctor"),Nurse("Nurse"),Receptionist("Receptionist"),
             Donor("Donor"),Patient("Patient"),
             CharityAdmin("CharityAdmin"),Manager("Manager"),Volunteer("Volunteer"),
             BloodBankAdmin("BloodbankAdmin"),BloodBankIncharge("BloodbankIncharge"),;
-
+            
+    private String val;
     private RoleType(String val){
         this.val=val;
     }
