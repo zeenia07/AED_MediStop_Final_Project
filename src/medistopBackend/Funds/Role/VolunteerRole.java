b55a6a9fe2c1f1f095a27d68a27daaf584ee5b51
Virendra Singh Rathore <rathore26@northeastern.edu>
2021-12-10 16:31:57 -0500

    updated funding role class

---
+++ src/medistopBackend/Funds/Role/VolunteerRole.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package medistopBackend.Funds.Role;
+import medistopBackend.Funds.Organisation.FundsOrganisationVolunteer;
+import medistopBackend.EcoSystem;
+import medistopBackend.Enterprise.FundingEnterprise;
+import medistopBackend.Enterprise.Enterprise;
+import medistopBackend.Network.Network;
+import medistopBackend.Organisation.Organisation;
+import medistopBackend.Role.Role;
+import medistopBackend.UserAccount.UserAccount;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author virendra
+ */
+public class VolunteerRole extends Role{
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organisation organisation, Enterprise enterprise,Network network, EcoSystem business)
+    {
+        //need to write the code here
+        return null;
+    }
+}
