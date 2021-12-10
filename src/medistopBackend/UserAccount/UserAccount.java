94edb46670bc40fe97c458a32179f8897e65c573
Zeenia Singla <singla.z@northeastern.edu>
2021-12-10 01:14:24 -0600

    UserAccount getters setters created

--- src/medistopBackend/UserAccount/UserAccount.java
+++ src/medistopBackend/UserAccount/UserAccount.java
@@ -5,9 +5,11 @@
  */
 package medistopBackend.UserAccount;
 
+import medistopBackend.Employee.Employee;
 import medistopBackend.Role.Role;
 import medistopBackend.UserData.DonorData;
 import medistopBackend.UserData.PatientData;
+import medistopBackend.WorkQueue.WorkQueue;
 
 /**
  *
@@ -21,5 +23,72 @@ public class UserAccount {
     private DonorData donor;
     private Role role;
     private WorkQueue workQueue;
+
+    public UserAccount() {
+       workQueue = new WorkQueue();
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public PatientData getPatient() {
+        return patient;
+    }
+
+    public void setPatient(PatientData patient) {
+        this.patient = patient;
+    }
+
+    public DonorData getDonor() {
+        return donor;
+    }
+
+    public void setDonor(DonorData donor) {
+        this.donor = donor;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public WorkQueue getWorkQueue() {
+        return workQueue;
+    }
+
+    public void setWorkQueue(WorkQueue workQueue) {
+        this.workQueue = workQueue;
+    }
+
+    @Override
+    public String toString() {
+        return username; //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
     
 }
