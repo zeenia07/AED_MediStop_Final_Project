48552cfd4e2cabcb3ead7e822a638e01445f3b16
Virendra Singh Rathore <rathore26@northeastern.edu>
2021-12-10 15:01:42 -0500

    implemented organisation and organisationDirectory

--- src/medistopBackend/Organisation/Organisation.java
+++ src/medistopBackend/Organisation/Organisation.java
@@ -5,7 +5,10 @@
  */
 package medistopBackend.Organisation;
 
-
+import medistopBackend.Employee.EmployeeDirectory;
+import medistopBackend.Role.Role;
+import medistopBackend.UserAccount.UserAccountDirectory;
+import medistopBackend.WorkQueue.WorkQueue;
 import java.util.ArrayList;
 
 /**
@@ -13,6 +16,122 @@ import java.util.ArrayList;
  * @author Virendra Rathore
  */
 public abstract class Organisation {
+    private String name;
+    private WorkQueue workQueue;
+    private EmployeeDirectory employeeDirectory;
+    private UserAccountDirectory userAccountDirectory;
+    private int organizationID;
+    private static int counter=0;
+    
+    public enum Type{
+        HospitalAdmin("Hospital Admin Organisation"), 
+        Doctor("Doctor Organisation"), 
+        Assistant("Assistant Organisation"),
+        Attendent("Attendent Organisation"),
+        Patient("Patient Organisation"),
+        Donor("Donor Organisation"),
+        FunderAdmin("Funder Admin Organisation"),
+        Manager("Manager Organisation"),
+        Volunteer("Volunteer Organisation"),
+        BloodbankAdmin("BloodBank Admin Organisation"),
+        BloodBankHead("BloodBankHead");
+        
+        private String value;
+        private Type(String value) {
+            this.value = value;
+        }
+        public String getValue() {
+            return value;
+        }
 
+        public static Type getHospitalAdmin() {
+            return HospitalAdmin;
+        }
+
+        public static Type getDoctor() {
+            return Doctor;
+        }
+
+        public static Type getReceptionist() {
+            return Assistant;
+        }
+
+        public static Type getNurse() {
+            return Attendent;
+        }
+
+        public static Type getPatient() {
+            return Patient;
+        }
+
+        public static Type getDonor() {
+            return Donor;
+        }
+
+        public static Type getCharityAdmin() {
+            return FunderAdmin;
+        }
+
+        public static Type getManager() {
+            return Manager;
+        }
+
+        public static Type getVolunteer() {
+            return Volunteer;
+        }
+
+        public static Type getBloodbankAdmin() {
+            return BloodbankAdmin;
+        }
+
+        public static Type getBloodBankIncharge() {
+            return BloodBankHead;
+        }
+        
+    }
+
+    public Organisation(String name) {
+        this.name = name;
+        workQueue = new WorkQueue();
+        employeeDirectory = new EmployeeDirectory();
+        userAccountDirectory = new UserAccountDirectory();
+        organizationID = counter;
+        ++counter;
+    }
+
+    public abstract ArrayList<Role> getSupportedRole();
+    
+    public UserAccountDirectory getUserAccountDirectory() {
+        return userAccountDirectory;
+    }
+
+    public int getOrganizationID() {
+        return organizationID;
+    }
+
+    public EmployeeDirectory getEmployeeDirectory() {
+        return employeeDirectory;
+    }
+    
+    public String getName() {
+        return name;
+    }
+
+    public WorkQueue getWorkQueue() {
+        return workQueue;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setWorkQueue(WorkQueue workQueue) {
+        this.workQueue = workQueue;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
    
 }
--- src/medistopBackend/Organisation/OrganisationDirectory.java
+++ src/medistopBackend/Organisation/OrganisationDirectory.java
@@ -6,6 +6,10 @@
 package medistopBackend.Organisation;
 
 
+import medistopBackend.Organisation.Organisation.Type;
+import medistopBackend.User.Organisation.DonorOrganisation;
+import medistopBackend.User.Organisation.PatientOrganisation;
+import medistopBackend.UserAccount.UserAccount;
 import java.util.ArrayList;
 
 /**
@@ -13,5 +17,77 @@ import java.util.ArrayList;
  * @author Virendra Rathore
  */
 public class OrganisationDirectory {
+    private ArrayList<Organisation> organisationList;
+
+    public OrganisationDirectory() {
+        organisationList = new ArrayList();
+    }
+
+    public ArrayList<Organisation> getOrganizationList() {
+        return organisationList;
+    }
     
+    
+    public Organisation createOrganisation(Type type){
+        Organisation organisation = null;
+        if (type.getValue().equals(Type.Doctor.getValue()))
+        {
+            organisation = new DoctorOrganisation();
+            organisationList.add(organisation);
+        }
+        
+        else if (type.getValue().equals(Type.Patient.getValue()))
+        {
+            organisation = new PatientOrganisation();
+            organisationList.add(organisation);
+        }
+        
+        else if(type.getValue().equals(Type.Donor.getValue()))
+        {
+            organisation = new DonorOrganisation();
+            organisationList.add(organisation);
+        }
+        else if(type.getValue().equals(Type.HospitalAdmin.getValue()))
+        {
+            organisation = new HospitalAdminOrganisation();
+            organisationList.add(organisation);
+        }
+        else if(type.getValue().equals(Type.Assistant.getValue()))
+        {
+            organisation = new AssistantOrganisation();
+            organisationList.add(organisation);
+        }
+        else if(type.getValue().equals(Type.Attendent.getValue()))
+        {
+            organisation = new AttendentOrganisation();
+            organisationList.add(organisation);
+        }
+        else if(type.getValue().equals(Type.BloodbankAdmin.getValue()))
+        {
+            organisation = new BloodbankAdminOrganisation();
+            organisationList.add(organisation);
+        }
+        else if(type.getValue().equals(Type.BloodBankHead.getValue()))
+        {
+            organisation = new HeadOrganisation();
+            organisationList.add(organisation);
+        }
+        else if(type.getValue().equals(Type.FunderAdmin.getValue()))
+        {
+            organisation = new FunderAdminOrganisation();
+            organisationList.add(organisation);
+        }
+        else if(type.getValue().equals(Type.Volunteer.getValue()))
+        {
+            organisation = new VolunteerOrganisation();
+            organisationList.add(organisation);
+        }
+        else if(type.getValue().equals(Type.Manager.getValue()))
+        {
+            organisation = new ManagerOrganisation();
+            organisationList.add(organisation);
+        }
+        
+        return organisation;
+    }
 }
