a74d73678034f41e1bd300ba97135146d9c24c6d
Zeenia Singla <singla.z@northeastern.edu>
2021-12-11 18:37:04 -0600

    AttendantWorkAreaJPanel created

--- src/medistopBackend/ConfigureASystem.java
+++ src/medistopBackend/ConfigureASystem.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -25,32 +25,3 @@ public class ConfigureASystem
         return system;
     }
 }
-=======
-///*
-// * To change this license header, choose License Headers in Project Properties.
-// * To change this template file, choose Tools | Templates
-// * and open the template in the editor.
-// */
-//package medistopBackend;
-//
-//import Buisness.Employee.Employee;
-//import Buisness.Role.SystemAdminRole;
-//import Buisness.UserAccount.UserAccountDirectory;
-//import Buisness.UserAccount.UserAccount;
-//
-///**
-// *
-// * @author manor
-// */
-//public class ConfigureASystem
-//{
-//    public static EcoSystem configure()
-//    {
-//        EcoSystem system = EcoSystem.getInstance();
-//        Employee employee = system.getEmployeeDirectory().createEmployee("sysadmin");
-//        UserAccount ua = system.getUserAccountDirectory().createUserAccount("sysadmin", "sysadmin", employee,new SystemAdminRole());
-//
-//        return system;
-//    }
-//}
->>>>>>> 10c99c85b400b2b979e09e02f10cd172d9d6032e
